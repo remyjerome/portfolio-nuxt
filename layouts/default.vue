01093880aa8b8025cc81f2e359f3a40eedd72da9
Remy JEROME <r.jerome@FIDUCIAL.DOM>
2018-09-11 12:58:48 +0200

    import all css + nav + home page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,36 +1,40 @@
 <template>
-  <div id="home">
-    <div id="nav_bar">
-        <a class="logo" href="">
-            <img src="https://via.placeholder.com/24x32" alt="">
-            <img src="https://via.placeholder.com/24x9" alt="">
-        </a>
-        <nav>
-            <a href="">
-                <i>#1</i>
+    <div id="home">
+        <div id="nav_bar">
+            <a class="logo" href="">
+                <img src="https://via.placeholder.com/24x32" alt="">
+                <img src="https://via.placeholder.com/24x9" alt="">
             </a>
-            <a href="">
-                <i>#2</i>
-            </a>
-            <a href="">
-                <i>#3</i>
-            </a>
-            <a href="">
-                <i>#4</i>
-            </a>
-            <a href="">
-                <i>#5</i>
-            </a>
-        </nav>
-        <ul></ul>
+            <nav>
+                <nuxt-link to="/">
+                    <span class="lnr lnr-home"></span>
+                </nuxt-link>
+                <nuxt-link to="/about">
+                    <span class="lnr lnr-user"></span>
+                </nuxt-link>
+                <nuxt-link to="/skills">
+                    <span class="lnr lnr-cog"></span>
+                </nuxt-link>
+                <nuxt-link to="/portfolio">
+                    <span class="lnr lnr-layers"></span>
+                </nuxt-link>
+                <nuxt-link to="/contact">
+                    <span class="lnr lnr-envelope"></span>
+                </nuxt-link>
+            </nav>
+            <ul></ul>
+        </div>
+        <div id="page">
+            <nuxt/>
+        </div>
     </div>
-    <div id="page">
-      <nuxt/>
-    </div>
-  </div>
 </template>
-
+<script>
+  export default {
+    components: {
+    }
+  }
+</script>
 <style lang="scss">
-@import '../assets/scss/main.scss';
 </style>
 
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,29 +1,22 @@
 module.exports = {
-  /*
-  ** Headers of the page
-  */
   head: {
-    title: 'rj',
+    title: 'RJ - Web Developper / Front End JS',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'portfolio' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href:'https://cdn.linearicons.com/free/1.0.0/icon-font.min.css' },
+      { rel: 'stylesheet', href:'https://unpkg.com/normalize.css@8.0.0/normalize.css' }
     ]
   },
-  /*
-  ** Customize the progress bar color
-  */
+
   loading: { color: '#3B8070' },
-  /*
-  ** Build configuration
-  */
+
   build: {
-    /*
-    ** Run ESLint on save
-    */
+
     extend (config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
@@ -35,8 +28,13 @@ module.exports = {
       }
     }
   },
+
   css: [
     '@/assets/scss/main.scss'
-  ]
+  ],
+
+  router: {
+    linkActiveClass: 'active-link'
+  }
 }
 
